afe49f735245c2b401071c6e764c2b5793179978
Sadupa Wijeratne <sadupag@hsenidmobile.com>
2016-07-26 15:43:28 +0530

    Renamed variable

--- integration/deploy.sh
+++ integration/deploy.sh
@@ -2,10 +2,10 @@
 # This script can be used to automatically build and deploy this project
 # Add following lines to your ~/.bash_profile
 # export TOMCAT_PATH=path to tomcat without ending slash
-# export SERVICES_HUB_PATH=path to project without ending slash
+# export SERVICE_HUB_PATH=path to project without ending slash
 
 sh $TOMCAT_PATH/bin/catalina.sh stop
-cd $SERVICES_HUB_PATH
+cd $SERVICE_HUB_PATH
 mvn clean install -DskipTests -o
 
 rm -r $TOMCAT_PATH/webapps/service-hub*
