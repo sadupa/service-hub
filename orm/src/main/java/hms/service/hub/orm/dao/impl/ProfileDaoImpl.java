346ff22c79c5f5ed138c93761cf1e61c53183acb
Udara <udara.seneviratne@hsenidmobile.com>
2016-07-26 16:46:39 +0530

    included profile dao, daoImpl, service, serviceImpl

---
+++ orm/src/main/java/hms/service/hub/orm/dao/impl/ProfileDaoImpl.java
@@ -0,0 +1,10 @@
+package hms.service.hub.orm.dao.impl;
+
+import hms.service.hub.orm.dao.ProfileDao;
+import hms.service.hub.orm.model.Profile;
+
+/**
+ * Created by udara on 7/26/16.
+ */
+public class ProfileDaoImpl extends UniversalDaoImpl<Profile> implements ProfileDao{
+}
