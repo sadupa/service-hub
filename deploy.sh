9bf4fe51806bdbc34fec68d5794e0eb3d79d6420
Kusala <kusala.rajakaruna@gmail.com>
2016-07-26 16:40:21 +0530

    Changed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 mvn clean install
 sh $TOMCAT_PATH/bin/shutdown.sh
 rm $TOMCAT_PATH/webapps/service-hub*
-cp /home/kusala/devinda/services-hub/web/target/service-hub.war $TOMCAT_PATH/webapps
+cp ./web/target/service-hub.war $TOMCAT_PATH/webapps
 sh $TOMCAT_PATH/bin/startup.sh
 tail -f $TOMCAT_PATH/logs/catalina.out
 
