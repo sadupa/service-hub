ed38fa976fa474cfcd926aa75d61d15c0676ad76
chathura <chathura.alahakoon@hsenidmobile.com>
2016-07-27 14:46:05 +0530

    initial db sql file added

---
+++ sql/initial_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO area VALUES
+  (1,'Kandy'),
+  (2,'Colombo'),
+  (3,'Jaffna'),
+  (4,'Kilinochchi'),
+  (5,'Mannar'),
+  (6,'Mullaitivu'),
+  (7,'Vavuniya'),
+  (8,'Puttalam'),
+  (9,'Kurunegala'),
+  (10,'Gampaha'),
+  (11,'Kalutara'),
+  (12,'Anuradhapura'),
+  (13,'Polonnaruwa');
+
+INSERT INTO category VALUES
+  (1,'Photography'),
+  (2,'Tourism'),
+  (3,'Private Cab'),
+  (4,'Catering'),
+  (5,'Entertainment'),
+  (6,'Electrician'),
+  (7,'Plumbing'),
+  (8,'Computer Repair'),
+  (9,'Baby Sitting'),
+  (10,'Painting'),
+  (11,'Car Mechanic');
